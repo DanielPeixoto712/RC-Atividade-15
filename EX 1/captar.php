93bd1a0b14e71d3c4328f098f825817600bce481
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-11-10 17:25:19 +0000

    Update captar.php, formulario.html, and 2 more files...

---
+++ EX 1/captar.php
@@ -0,0 +1,11 @@
+<?php
+$Numero1=$_POST["Num1"];
+$Numero2=$_POST["Num2"];
+$Numero3=$_POST["Num3"];
+$Numero4=$_POST["Num4"];
+
+
+$soma=$Numero1+$Numero2+$Numero3+$Numero4;
+echo "Resultado da soma: " .$soma. "<br>";
+
+?>
---
+++ EX 2/captar.php
@@ -0,0 +1,19 @@
+<?php
+$Numero1=$_POST["Num1"];
+$Numero2=$_POST["Num2"];
+
+
+
+
+
+
+
+
+$soma=$Numero1+$Numero2;
+$subtraçao=$Numero1-$Numero2;
+$divisao=$Numero1/$Numero2;
+$multiplicacao=$Numero1*$Numero2;
+echo "Resultado da soma: " .$soma.  "<br>";
+echo "Resultado subtração: " .$subtraçao.  "<br>";
+echo "Resultado da divisão: " .$divisao.  "<br>";
+echo "Resultado da multiplicação: " .$multiplicacao.  "<br>";
