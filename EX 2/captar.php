9830e1e5b7b03fd0600983ebfbdf9cecdfd94504
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-11-13 11:23:03 +0000

    Update captar.php

--- EX 2/captar.php
+++ EX 2/captar.php
@@ -2,20 +2,46 @@
 $Numero1=$_POST["Num1"];
 $Numero2=$_POST["Num2"];
 
-function som($num1, $num2){
-	$soma=0;
-	$soma=$num1+$num2;
 
-}
-echo "Resultado da soma: " .soma.  "<br>";
-
+$soma=$Numero1+$Numero2;
 $subtraçao=$Numero1-$Numero2;
 $divisao=$Numero1/$Numero2;
 $multiplicacao=$Numero1*$Numero2;
 
 
+function soma($Numero1, $Numero2){
+	$soma=0;
+	$soma=$Numero1+$Numero2;
+	return $soma;
+
+}
 
 
-echo "Resultado subtração: " .$subtraçao.  "<br>";
-echo "Resultado da divisão: " .$divisao.  "<br>";
-echo "Resultado da multiplicação: " .$multiplicacao.  "<br>";
+function sub($Numero1, $Numero2){
+	$subtraçao=0;
+	$subtraçao=$Numero1-$Numero2;
+	return $subtraçao;
+}
+
+
+
+function div($Numero1, $Numero2){
+	$divisao=0;
+	$divisao=$Numero1/$Numero2;
+	return $divisao;
+}
+
+
+function mult($Numero1, $Numero2){
+	$multiplicacao=0;
+	$multiplicacao=$Numero1*$Numero2;
+	return $multiplicacao;
+}
+
+echo "Resultado da soma: " .soma($Numero1, $Numero2). "<br>" ;
+
+echo "Resultado da subtração: " .sub($Numero1, $Numero2). "<br>";
+
+echo "Resultado da divisão: " .div($Numero1, $Numero2). "<br>";
+
+echo "Resultado da multiplicação: " .mult($Numero1, $Numero2).  "<br>";
